2d51a74ecb34a78d21dd750af0baa05e67f0570d
ahmadelbaz <ahmeed.elbaz@gmail.com>
2022-07-29 16:56:31 +0200

    Obtimized achievements model

--- lib/models/achievement.dart
+++ lib/models/achievement.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'database_model.dart';
 
+// Model for achievement
 class Achievement implements DatabaseModel {
   String id = '';
   String task = '';
@@ -13,7 +14,9 @@ class Achievement implements DatabaseModel {
     required this.hasDone,
   });
 
+  // Handle the data that is coming from db
   Achievement.fromMap(Map<String, dynamic> map) {
+    // Convert int to bool (cuz sqlite doesn't have bool so we use int instead)
     final newHasDone = map['hasDone'] == 0 ? false : true;
     id = map['id'];
     task = map['task'];
@@ -36,8 +39,10 @@ class Achievement implements DatabaseModel {
     return 'achievements';
   }
 
+  // Handle the data before storing it in db
   @override
   Map<String, dynamic>? toMap() {
+    // Convert bool to int to store it in db
     final newHasDone = hasDone ? 1 : 0;
     return {'id': id, 'task': task, 'price': price, 'hasDone': newHasDone};
   }
